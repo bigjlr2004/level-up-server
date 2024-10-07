3112de7a5a25848245119020dee02e5c3f036793
John Roberts <bigjlr2004@yahoo.com>
2024-10-07 16:17:27 -0500

    Added endpoint connectiosn for get all games and get all events

--- levelup/urls.py
+++ levelup/urls.py
@@ -16,10 +16,24 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from levelupapi.views import register_user, check_user
+from levelupapi.views import (
+    register_user,
+    check_user,
+    GameTypeView,
+    EventView,
+    GameView,
+)
+from django.conf.urls import include
+from rest_framework import routers
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r"gametypes", GameTypeView, "gametype")
+router.register(r"events", EventView, "event")
+router.register(r"games", GameView, "game")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("register", register_user),
     path("checkuser", check_user),
+    path("", include(router.urls)),
 ]
